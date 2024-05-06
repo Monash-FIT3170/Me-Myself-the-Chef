288d9ce7ec1f77abf606f1be1db19c9a7ef3b57e
tpan0022 <tpan0022@student.monash.edu>
2024-05-06 19:19:38 +1000

    spike: implemented a basic test api call using the Page 1 route

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const path = require("path");
-const React = require("react");
-const ReactDOMServer = require("react-dom/server");
-const NavBar = require("./my-app/frontend/src/components/NavBar.js").default;
+const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "frontend", "src", "views"));
 
 // Serve static files from the public directoryd
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use(express.json());
 
-// Define routes
-app.get("/recipe", (req, res) => {
-    // Render the NavBar component to a string
-    const navBarString = ReactDOMServer.renderToString(NavBar);
+
+// // Define routes
+// app.get("/recipe", (req, res) => {
+//     // Render the NavBar component to a string
+//     const navBarString = ReactDOMServer.renderToString(NavBar);
     
-    // Pass the NavBar component as a prop to the recipe view
-    res.render("recipe", { navBar: navBarString });
-});
+//     // Pass the NavBar component as a prop to the recipe view
+//     res.render("recipe", { navBar: navBarString });
+// });
+
+app.get("/api/page1data", async (req, res) => {
+    res.json([1,2,3,4,5])
+})
 
 // Start the server
 app.listen(PORT, () => {
