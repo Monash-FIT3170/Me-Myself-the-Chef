0825ee991a4ceaabe558c55e3f119734262c66e0
Yuhan Zhou <Yuhanzhou2000@gmail.com>
2024-05-14 13:25:07 +1000

    Display list of recipes in card view including title and image

--- frontend/src/components/RecipeCard.js
+++ frontend/src/components/RecipeCard.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/base.css'
+import {Link} from "react-router-dom";
 
-function RecipeCard({ recipeNumber }) {
+function RecipeCard({recipe}) {
+
    return (
        <div className="col-12 col-lg-6">
            <div className="card mb-3" style={{ maxWidth: '540px' }}>
-               <a href="#" className="card-link stretched-link"></a>
+               <Link to={{pathname: `/recipe`, state: {recipe: recipe}}} className="react_link">
+                   <a href="#" className="card-link stretched-link"></a>
+               </Link>
+
+               {/*<Link className="react_link" to="/recipe">*/}
+               {/*    <a href="#" className="card-link stretched-link"></a>*/}
+               {/*</Link>*/}
+               {/*<a href="#" className="card-link stretched-link"></a>*/}
                <div className="row g-0">
                    <div className="col-md-8">
                        <div className="card-body">
-                           <h5 className="card-title">Recipe {recipeNumber}
-                               <button type="button" className="btn btn-link text-danger"><i className="fa fa-heart" style={{ fontSize: '24px' }}></i></button>
+                           <h5 className="card-title">{recipe.title}
+                               {/*<button type="button" className="btn btn-link text-danger"><i className="fa fa-heart" style={{ fontSize: '24px' }}></i></button>*/}
                            </h5>
-                           <p className="card-text">You have all the ingredients</p>
+                           <p className="card-text">You are missing {recipe.missedIngredientCount} ingredients</p>
                        </div>
                    </div>
                    <div className="col-md-4">
-                       <img src="/static/images/pancake.jpg" className="img-fluid rounded-start" alt="..." />
+                       <img src={recipe.image} className="img-fluid rounded-start" alt="..." />
                    </div>
                </div>
            </div>
--- frontend/src/components/RecipePane.js
+++ frontend/src/components/RecipePane.js
@@ -4,17 +4,31 @@ import RecipeCard from "./RecipeCard";
 
 function RecipePane({ recipeList }) {
 
+    if (!recipeList) {
+        return <div className="col-md-9" style={{backgroundColor: '#ECECEC'}}>
+            <div className="row pt-5 text-center">
+                <h2>Loading</h2>
+            </div>
+            {/* Display recipe generation result */}
+            <div className="container">
+                <div className="row">
+                </div>
+            </div>
+        </div>
+    }
+
    return (
       <div className="col-md-9" style={{ backgroundColor: '#ECECEC' }}>
          <div className="row pt-5 text-center">
             <h2>Recipe Recommendations</h2>
-            <h5> You have 8 recommendations</h5>
+            <h5> You have {Object.keys(recipeList).length} recommendations</h5>
             <div className="horiz_line" style={{ backgroundColor: '#000000' }}></div>
          </div>
          {/* Display recipe generation result */}
          <div className="container">
             <div className="row">
-               {[...Array(8).keys()].map(i => <RecipeCard key={i + 1} recipeNumber={i + 1} />)}
+                {recipeList.map((recipe, index) => (<RecipeCard key={index + 1} recipe={recipe} />))}
+               {/*{[...Array(8).keys()].map(i => <RecipeCard key={i + 1} recipeNumber={i + 1} />)}*/}
             </div>
          </div>
       </div>
