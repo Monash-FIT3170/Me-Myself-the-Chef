86e57314e865ebc046bc28894f71362af01727fc
dtru0010 <163800064+danieltruongg@users.noreply.github.com>
2024-05-15 00:35:12 +1000

    Fully implemented Dietary Preferences to be stored into Local Storage

--- frontend/src/components/DietaryPreference.js
+++ frontend/src/components/DietaryPreference.js
@@ -4,19 +4,50 @@ import '../css/base.css'
 import DietaryPane from './DietaryPane';
 
 function DietaryPreference(){
-    
-    const [dietaryList, setDietaryList] = useState([
-        {name: "Gluten Free", state: ""},
-        {name: "Ketogenic", state: ""},
-        {name: "Vegetarian", state: ""},
-        {name: "Vegan", state: ""},
-        {name: "Pescetarian", state: ""},
-    ])
 
+    // base dietary list
+    // if you make changes to this after running, you must clear localStorage to see changes
+    const baseDietaryList = [
+        {id: 0, name: "Gluten Free", state: false},
+        {id: 1, name: "Ketogenic", state: false},
+        {id: 2, name: "Vegetarian", state: false},
+        {id: 3, name: "Vegan", state: false},
+        {id: 4, name: "Pescetarian", state: false},
+        ]
+    
+    // obtains the dietary list from localStorage or uses the base one
+    // sets the list as an object to handle in js
+    const [dietaryList, setDietaryList] = useState(() => {
+        const localValue = localStorage.getItem("DIETS")
+        if (localValue == null) return baseDietaryList
+
+        return JSON.parse(localValue)
+    })
+
+    // converts object into JSON and sets it into localStorage
+    useEffect(() => {
+        localStorage.setItem("DIETS", JSON.stringify(dietaryList))
+    }, [dietaryList])
+
+
+    // updates the dietary list based on the checkbox checked status
     const updateDietary = (event) => {
-        const { name, state } = event.target
-        setDietaryList({...dietaryList,[name]: state});
+        const id = event.target.id
+        let checked = event.target.checked
+
+        const updatedObj = {...dietaryList[id], state: checked}
+
+        setDietaryList(
+            dietaryList.map( obj =>
+                obj.id === updatedObj.id ? updatedObj : obj
+            )
+        )
     }
+
+    return (
+        <DietaryPane dietaryList = {dietaryList} updateDietary={updateDietary} />
+    )
+
 }
 
 export default DietaryPreference
