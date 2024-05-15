1376b06a8a6ca81bd00ced64192592ae055dfbeb
Xiao Hao Zheng <xzhe0014@student.monash.edu>
2024-05-15 13:29:43 +1000

    feat: Updated sign in/logout on NavBar to be dynamic

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,62 +1,65 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import '../css/base.css'
-
+import React, { useContext } from 'react';
+import { Link, useLocation } from "react-router-dom";
+import { AuthContext } from '../context/AuthContext';
+import '../css/base.css';
 
 function Navbar() {
+    const { isLoggedIn, logout } = useContext(AuthContext);
+    const location = useLocation();
+
+    const handleLogout = () => {
+        logout();
+        window.location.reload();
+    };
+
     return (
         <nav className="navbar navbar-expand-md bg-body-tertiary">
             <div className="container-fluid">
                 
                 <Link className='react_link' to="/ingredients">
-                    <a className="navbar-brand px-2" href="#">
-                        {/* TODO: make an actual logo for this text */}
-                        
+                    <span className="navbar-brand px-2">
                         Me Myself the Chef
-                        <img src="/static/images/logo_small.png"  height="50" alt="Logo" className="d-inline-block ms-2" />
-                    </a>
+                        <img src="/static/images/logo_small.png" height="50" alt="Logo" className="d-inline-block ms-2" />
+                    </span>
                 </Link>
 
-                {/* This button appears when the window is too small: hamburger button for showing the navbar items*/}
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    
-                    {/* The header links */}
                     <ul className="navbar-nav ms-auto">
-
                         <li className="nav-item px-2">
                             <Link className='react_link' to="/ingredients">
-                                <a className="nav-link" href="#">Recipe Search</a>
+                                <span className="nav-link">Recipe Search</span>
                             </Link>
-
                         </li>
 
                         <li className="nav-item px-2">
                             <Link className='react_link' to="/ingredients">
-                                <a className="nav-link" href="#">Saved Recipes</a>
+                                <span className="nav-link">Saved Recipes</span>
                             </Link>
                         </li>
 
                         <li className="nav-item px-2">
                             <Link className='react_link' to="/preferences">
-                                <a className="nav-link" href="#">Preferences</a>
+                                <span className="nav-link">Preferences</span>
                             </Link>
                         </li>
-                        
-                        {/* TODO: possibly some dynamic imagery to show whether they're signed in or not*/}
-                        {/* Also the login favicon currently is black, doesn't look amazing*/}
-                        <Link className='react_link' to="/login">
-                            <a className="navbar-brand px-4" href="#">
-                                <img src="/static/images/person-circle.svg" alt="Login" width="30" height="30" />
-                            </a>
-                        </Link>
-                        
+
+                        {isLoggedIn ? (
+                            <li className="nav-item px-2">
+                                <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
+                            </li>
+                        ) : (
+                            <li className="nav-item px-2">
+                                <Link to="/login">
+                                    <button className="btn btn-primary">Sign In</button>
+                                </Link>
+                            </li>
+                        )}
                     </ul>
                 </div>
-
             </div>
         </nav>
     );
