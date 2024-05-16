809d0444eea8322727fff58fbbfd26b06cef6777
Alana Vanzo <63185241+Alanavanzo@users.noreply.github.com>
2024-05-16 13:16:27 +1000

    working on complesSearch call - error occuring on line 122 (setting recipe list)

--- frontend/src/views/RecipeRecommendation.js
+++ frontend/src/views/RecipeRecommendation.js
@@ -4,7 +4,7 @@ import IngredientsRecipePane from '../components/IngredientsRecipePane';
 import RecipePane from '../components/RecipePane';
 import { useState, useEffect } from "react";
 import axios from "axios";
-const API_KEY = "09861c68c07140d8a96e353c8d4f86cc";
+const API_KEY = "4cb545ba636c422990fb2f1cfc567c69";     // Alana's API key 
 
 // function to call Spoonacular API and generate recipes based off of ingredients and preferences
 function RecipeRecommendation() {
@@ -50,7 +50,7 @@ function RecipeRecommendation() {
             for (let i = 0; i < dietaries.length; i++) {
                 //// this will depend on how preferences are being stores 
                 
-                if (dietaries[i].state == "True"){
+                if (dietaries[i].state === "True"){
                     dietaryString += dietaries[i].name;
                 }
                 // check to add ",+"
@@ -74,17 +74,25 @@ function RecipeRecommendation() {
                 //// try complex search https://api.spoonacular.com/recipes/complexSearch
                 /// params: includeIngredients, excludeIngredients
                 const response = await axios.get("https://api.spoonacular.com/recipes/complexSearch", {
+                //const response = await axios.get("https://api.spoonacular.com/recipes/findByIngredients", {
                     params: {
-                        apiKey: API_KEY,
-                        ingredients: ingredientString,
-                        number: 3,   // number of recipes to return 
-                        diet: dietaryString,
-                        maxReadyTime: maxPrepTime,
-                        intolerances: cannot_eat
-                        /// can potentially add --> addRecipeInformation: True to reduce the number of API calls required 
+                       apiKey: API_KEY,
+                        //query: "pasta",
+                        includeIngredients: ingredientString,
+                        //number: 2,
+                        //ingredients: ingredientString, //--> FOR INGREDIENT SEARCH ONLY 
+                        number: 3   // number of recipes to return 
+
+                        // IF USING COMPLEX SEARCH 
+
+                        //diet: dietaryString,
+                        //maxReadyTime: maxPrepTime,
+                        //intolerances: cannot_eat
+                        ///// can potentially add --> addRecipeInformation: True to reduce the number of API calls required 
                         // ignorePantry parameter can be used to ignore flour, salt, etc. 
                     }
-                })
+                }
+            )
                 setRecipeList(response.data);
                 console.log(response.data);
             } catch (error) {
@@ -92,7 +100,10 @@ function RecipeRecommendation() {
             }
         };
 		
-        // check if data has been collected already
+        // check if data has been collected already /// Why is this needed? It is inside the use effect anyway so we can't get here withut fetching the data 
+        //if (recipeList !== null){
+        //    console.log(recipeList);
+        //}
         if (recipeList == null) {
             fetchData();
         } else {
@@ -107,6 +118,7 @@ function RecipeRecommendation() {
             <IngredientsRecipePane ingredientList={ingredientList}/>
 
             {/* Display Recipe Pane */}
+            {/* THIS IS THE PART THAT ISN't WORKING */}
             <RecipePane recipeList={recipeList}/>
 
         </div>
