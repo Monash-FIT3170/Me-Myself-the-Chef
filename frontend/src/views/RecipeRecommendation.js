2be4d459c17e480d4b6185abb2b79a3264886030
Alana Vanzo <63185241+Alanavanzo@users.noreply.github.com>
2024-05-18 11:55:29 +1000

    works with test API call, need to adjust parameter input to fix bug and allow dynamic preference input

--- frontend/src/views/RecipeRecommendation.js
+++ frontend/src/views/RecipeRecommendation.js
@@ -25,12 +25,13 @@ function RecipeRecommendation() {
     });
 
     const [recipeList, setRecipeList] = useState();
+    const [recipeNum, setRecipeNum] = useState();
 
     // is called everytime the page reloads/renders
     useEffect(() => {
         localStorage.setItem("INGREDIENTS", JSON.stringify(ingredientList))
     }, [ingredientList]);
-
+    
     useEffect(() => {
         const fetchData = async () => {
             let ingredientString = "";      // need to pass ingredients into API as one string 
@@ -72,34 +73,44 @@ function RecipeRecommendation() {
                 // original search "https://api.spoonacular.com/recipes/findByIngredients"
                 //// try complex search https://api.spoonacular.com/recipes/complexSearch
                 /// params: includeIngredients, excludeIngredients
-                const response = await axios.get("https://api.spoonacular.com/recipes/complexSearch", {
+                const response = await axios.get("https://api.spoonacular.com/recipes/complexSearch?apiKey=4cb545ba636c422990fb2f1cfc567c69&query=pasta&maxFat=25&number=5") 
+                //const response = await axios.get("https://api.spoonacular.com/recipes/complexSearch", {
                 //const response = await axios.get("https://api.spoonacular.com/recipes/findByIngredients", {
+                    /*
                     params: {
                        apiKey: API_KEY,
-                        //query: "pasta",
+                       limitLicense: false,
+                        query: "pasta",
+                        maxFat: 25, 
+                        number: 5
                         includeIngredients: ingredientString,
                         //addRecipeInformation: true,
                         //number: 2,
                         //ingredients: ingredientString, //--> FOR INGREDIENT SEARCH ONLY 
-                        number: 3,   // number of recipes to return 
+                        number: 3//,   // number of recipes to return 
 
                         // IF USING COMPLEX SEARCH --- TEST DATA 
 
-                        diet: dietaryString,
-                        maxReadyTime: maxPrepTime,
-                        intolerances: cannot_eat
+                        //diet: dietaryString,
+                        //maxReadyTime: maxPrepTime,
+                        //intolerances: cannot_eat
                         ///// can potentially add --> addRecipeInformation: True to reduce the number of API calls required 
                         // ignorePantry parameter can be used to ignore flour, salt, etc. 
                     }
                 }
+                
             )
-                setRecipeList(response.data);
-                console.log(response.data);
+            */
+            
+                setRecipeList(response.data.results);//; use for complex search 
+                setRecipeNum(response.data.totalResults) //test for complex search
+                //setRecipeList(response.data)
+                console.log(response.data.results);
             } catch (error) {
                 console.log(error);
             }
         };
-		
+        
         // check if data has been collected already /// 
         //if (recipeList !== null){
         //    console.log(recipeList);
@@ -109,21 +120,32 @@ function RecipeRecommendation() {
         } else {
             console.log(recipeList);
         }
-	}, []);
-
+    }, []); /// changing to {} captures the object 
 
     return (
         <div className="row flex-fill">
             {/* Ingredients Pane */}
-            <IngredientsRecipePane ingredientList={ingredientList}/>
+            {/*<IngredientsRecipePane ingredientList={ingredientList}/>*/}
 
             {/* Display Recipe Pane */}
             {/* THIS IS THE PART THAT ISN't WORKING --> problem was that new output is object not list */} 
-            <ComplexRecipePane recipeList={recipeList.results}/>
+            {/*<ComplexRecipePane recipeList={recipeList}/>*/}
+            {recipeNum}
+            {/*recipeList*/}
+            {recipeList.map(item => (
+                <div key={item.id}>{item.title}</div>
+                ))}
 
-        </div>
 
+                
+            
+        
+            {/*recipeList*/}
+            {/*{recipeList}*/}
+            </div>
     );
 }
 
 export default RecipeRecommendation;
+
+
