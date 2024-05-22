7959e9f8197ab805b88daf5d9c835064b39c0e57
Alana Vanzo <63185241+Alanavanzo@users.noreply.github.com>
2024-05-22 12:53:12 +1000

    changed heading to improve ability

--- frontend/src/components/IngredientSearch.js
+++ frontend/src/components/IngredientSearch.js
@@ -17,7 +17,7 @@ function IngredientSearch({addIngredient}) {
             <div className="container">
                 
                 <div className="row search_section">
-                    <h1 className="heading">Make your ingredients</h1>
+                    <h1 className="heading">Select your ingredients</h1>
                 </div>
 
                 <div className="row mt-4">
